5d6ebf304ff46b81c51db045a2c682f00fb2b054
Ali Nayebi <elderekkoo@gmail.com>
2023-07-22 13:52:20 +0330

    dinamic Routes and useParams

--- react-router-dom/src/App.js
+++ react-router-dom/src/App.js
@@ -1,14 +1,16 @@
 import { Link, Route, Routes } from "react-router-dom";
-// Routes component wrap around our individual or group of route componnts which each define a path to a different page in our website.
-// every Route needs a path which is like a url that defines it's location and the component that is going trough that path is defined in the element.
-// the root path of the application is sinmple slash
 import "./App.css";
 import { Home } from "./pages/Home";
 import { BookList } from "./pages/BookList";
+import { Book } from "./pages/Book";
+import { NewBook } from "./pages/NewBook";
+// Routes component wrap around our individual or group of route components which each define a path to a different page in our website.
+// every Route needs a path which is like a url that defines it's location and the component that is going trough that path is defined in the element.
+// the root path of the application is sinmple slash
 // we can also do navigation with react router and to do that we use Link component
 // Link component essentionaly gonna replace all the anchor tags in our application
 // Link component is technically an anchor tag but it's going to be used within react-router to automatically swap things in the application without refreshing the entire page
-
+// we can define a route that has a custome parameter inside of it which is going to be dinamic and able to be changed eg /books/:id this says that we have a url that goes /books and then there is something after it, in this case a id.
 function App() {
   return (
     <>
@@ -25,6 +27,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/books" element={<BookList />} />
+        <Route path="/books/:id" element={<Book />} />
+        <Route path="/books/new" element={<NewBook />} />
+        {/* these last two routes tecknically match beacause new can be a id so how does react router know which one to use, well back before version 6 react-router used to go top to bottom and match the first one but in version 6 react router first matches all the hard coded routes and then if nothing marches goes trough the dinamic Routes */}
       </Routes>
     </>
   );
--- react-router-dom/src/pages/Book.js
+++ react-router-dom/src/pages/Book.js
@@ -1,3 +1,7 @@
+import { useParams } from "react-router-dom";
+// to recap useParams hook is taking the custome dinamic parameter in our Route which is id and then we assign it to {id} and the placed it in h1
+// with the :id parameter we are saying that whatever comes after : replace it with id and then we take it from useParams and pass it in h1
 export function Book() {
-  return <h1>Book</h1>;
+  const { id } = useParams();
+  return <h1>Book {id}</h1>;
 }
